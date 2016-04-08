d0c0fb89777f229746d9d2c9c1c5228bd3e5da76
mauro-takeda-movile <mauro.takeda@movile.com>
2016-04-08 11:46:45 -0300

    First class setup

--- src/main/scala/com/scala/classes/ClassOne.scala
+++
@@ -1,5 +0,0 @@
-package com.scala.classes
-
-class ClassOne {
-  
-}
---
+++ src/main/scala/com/scala/classes/HelloWorld.scala
@@ -0,0 +1,55 @@
+package com.scala.classes
+
+class HelloWorld {
+  
+}
+
+
+//TODO: declaracao de classes
+//TODO: object
+//TODO: método "main" com a mensagem Hello World
+//TODO: declaracao de variaveis (val e var)
+//TODO: construtores e valores default
+//TODO: declaracao de métodos
+//TODO: no "return"
+//TODO: tudo é um objeto, inclusive as funções
+//TODO: mais de uma classe por arquivo
+//TODO: arquivo e classe nao precisam ter o mesmo nome
+//
+//TODO: task:
+/* 
+ * Implementar a Classe MyInt que recebe um inteiro e possui as seguintes funções
+ * add(MyInt): MyInt
+ * add(Int): MyInt
+ * sub(MyInt): MyInt
+ * sub(Int): MyInt   
+ * mult(MyInt): MyInt
+ * mult(Int): MyInt
+ * div(MyInt): MyInt
+ * div(Int): MyInt
+ * 
+ * Imprimir o resultado de 1 add 2 mult 3, com e sem os parenteses
+ */
+//TODO: extra task :
+/*
+ * criar outros metodos com os operadores tradicionais (*, -, /)
+ * verificar o comportamento da mesma expressão com e sem parenteses. Por que isso acontece?
+ * ver o mesmo com inteiros tradicionais 
+ * 
+ * repetir com métodos terminados com :
+ */
+ //TODO: extra task 2 :
+/*
+ * usar implicits e trabalhar com inteiros diretamente ao invés do MyInt (com os métodos add, sub, mult, div)
+ * 
+ */
+ 
+//TODO: proximas aulas
+//TODO: Data Structures: Lists and Maps
+//TODO: Flow Control: if, for, map, flatMap, forEach, fold, Pattern Matching (principles), recursion and tail recursio
+//TODO: Case Class
+//TODO: Companion Objects
+//TODO: More Pattern Matching 
+//TODO: apply method
+//TODO: Optional (Some, None)
+//TODO:
